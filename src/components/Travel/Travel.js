073a055d36f3db2e2f9a90c51828997050fa9045
BK Uddin <1bkhossain@gmail.com>
2021-09-26 01:32:49 +0600

    Defined main section area

---
+++ src/components/Travel/Travel.js
@@ -0,0 +1,21 @@
+import "./Travel.css";
+
+import React from "react";
+
+const Travel = () => {
+  return (
+    <div>
+      <hr />
+      <div className="travel-container">
+        <div className="hotel-container">
+          <h1>All Hotels</h1>
+        </div>
+        <div className="cart-container">
+          <h2>Booking: </h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Travel;
